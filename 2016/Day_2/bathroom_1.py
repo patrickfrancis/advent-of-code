a22960ff22e3380cc05531792857aeddc90ad986
Patrick Francis <patrick.francis@us.abb.com>
2016-12-06 22:47:38 -0500

    Added solution for Day 2 part 1.

---
+++ 2016/Day_2/bathroom_1.py
@@ -0,0 +1,52 @@
+# Keypad
+# 1 2 3
+# 4 5 6
+# 7 8 9
+
+
+def do_single_move(start: int, direction: str) -> int:
+    if direction == "U":
+        if start <= 3:
+            return start
+        else:
+            return start - 3
+    if direction == "D":
+        if start >= 7:
+            return start
+        else:
+            return start + 3
+    if direction == "L":
+        if start % 3 == 1:
+            return start
+        else:
+            return start - 1
+    if direction == "R":
+        if start % 3 == 0:
+            return start
+        else:
+            return start + 1
+
+
+def do_line(start: int, moves: str) -> int:
+    prev = start
+    result = start
+    for move in moves:
+        result = do_single_move(prev, move)
+        prev = result
+    return result
+
+
+def read_bathroom_code(filename: str) -> list:
+    with open(filename) as fin:
+        return fin.readlines()
+
+
+if __name__ == "__main__":
+    #lines = ["ULL", "RRDDD", "LURDL", "UUUUD"]
+    lines = read_bathroom_code("day2_input.txt")
+    prev = 5
+    for line in lines:
+        line = line.strip()
+        result = do_line(prev, line)
+        print(result)
+        prev = result
