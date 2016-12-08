da3000b5c1c84c91beac19715880d1ee72ee9051
Patrick Francis <patrick.francis@us.abb.com>
2016-12-08 16:40:43 -0500

    Added solution for Day 6 part 2.

---
+++ 2016/Day_6/repetition_2.py
@@ -0,0 +1,13 @@
+from operator import itemgetter
+from collections import Counter, defaultdict
+
+
+if __name__ == "__main__":
+    counts = defaultdict(Counter)
+    with open("day6_input.txt") as fin:
+        for line in fin:
+            word = line.strip()
+            for index in range(len(word)):
+                counts[index].update(word[index])
+    letters = [pos_count.most_common()[-1] for pos, pos_count in sorted(counts.items(), key=itemgetter(0))]
+    print("".join([c[0] for c in letters]))
