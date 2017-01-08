a171b039132d88d597e03350ecbd803a19d4b51b
Patrick Francis <patrick.francis@us.abb.com>
2017-01-08 16:26:48 -0500

    Added solution for Day 14 part 2.

---
+++ 2016/Day_14/onetimepad_2.py
@@ -0,0 +1,48 @@
+import re
+from hashlib import md5
+
+triple_pattern = re.compile(r'([a-z0-9])\1\1')
+# five_pattern   = re.compile(r'([a-z0-9])\1\1\1\1')
+
+salt = "ngcjuoqr"
+# salt = "abc"
+hashes = {}
+
+
+def get_hash(index: int) -> str:
+    if index not in hashes:
+        hash_val = md5("{0}{1}".format(salt, index).encode('ascii')).hexdigest().lower()
+        for s in range(2016):
+            hash_val = md5(hash_val.encode('ascii')).hexdigest().lower()
+        hashes[index] = hash_val
+    return hashes[index]
+
+
+def is_key(index: int) -> bool:
+    m = triple_pattern.search(get_hash(index))
+    if not m:
+        return False
+    val = m.group(1)
+    # print("Index {0} has triple character: {1}".format(index, val*3))
+    search_string = val*5
+    for i in range(index + 1, index + 1001):
+        test_hash = get_hash(i)
+        if search_string in test_hash:
+            # print("Matching 5-string found in index {0}".format(i))
+            return True
+    return False
+
+
+if __name__ == "__main__":
+    # print(get_hash(0))
+    # print(is_key(5))
+    # print(is_key(10))
+    # print(is_key(22551))
+
+    key_indices = []
+    curr_index = 0
+    while len(key_indices) < 64:
+        if is_key(curr_index):
+            key_indices.append(curr_index)
+        curr_index += 1
+    print("64th key at index {0}".format(key_indices[63]))
