5f34fdb19756b1dec8adc5f0731355ca429a7382
Patrick Francis <patrick.francis@us.abb.com>
2017-01-09 09:28:26 -0500

    Added solution for Day 18 part 2.

---
+++ 2016/Day_18/rogue_2.py
@@ -0,0 +1,32 @@
+def is_trap(left: bool, center: bool, right: bool) -> bool:
+    if (left and center and not right) or (not left and center and right):
+        return True
+    elif (left and not center and not right) or (not left and not center and right):
+        return True
+    else:
+        return False
+
+
+def print_room(room):
+    for row_num in range(len(room)):
+        print("".join(["^" if t else "." for t in room[row_num]]))
+
+
+if __name__ == "__main__":
+    # room = [[False, False, True, True, False]]
+    with open("day18_input.txt") as fin:
+        start_row = [True if c == "^" else False for c in fin.readline().strip()]
+    room = [start_row]
+    max_rows = 400000
+
+    for row_num in range(max_rows - len(room)):
+        prev_row = room[-1]
+        new_row = []
+        for i in range(len(prev_row)):
+            left = prev_row[i-1] if i-1 >= 0 else False
+            center = prev_row[i]
+            right = prev_row[i+1] if i+1 < len(prev_row) else False
+            new_row.append(is_trap(left, center, right))
+        room.append(new_row)
+    # print_room(room)
+    print("Safe tiles = {0}".format(sum([not trap for row in room for trap in row])))
